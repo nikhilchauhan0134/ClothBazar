4958c128806967d4c199af2ad3d983bb6bbee9c8
suman kashyap <harshkumar262015@gmail.com>
2020-06-30 14:58:16 +0530

    Add project files.

---
+++ ClothBazar.Entites/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entites
+{
+    public class Class1
+    {
+    }
+}
